c54f93ad6a3e4407072215ec26e6e823f3da7b32
Christina Burchette <burchc426@gmail.com>
2018-12-17 23:49:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
-const allDrivers = []
+const allDrivers = [...drivers]
 function destructivelyAppendDriver() {
   drivers.push("Ralph")
 }
