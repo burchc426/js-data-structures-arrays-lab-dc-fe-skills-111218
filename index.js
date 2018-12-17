f77ed26aecf00364260d1631c4cee91a115fb7e8
Christina Burchette <burchc426@gmail.com>
2018-12-17 23:45:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function destructivelyAppendDriver() {
 function destructivelyPrependDriver() {
   drivers.unshift("Bob")
 }
-function destructivelyRemoveFirstDriver
+function destructivelyRemoveLastDriver() {
+  
+}
